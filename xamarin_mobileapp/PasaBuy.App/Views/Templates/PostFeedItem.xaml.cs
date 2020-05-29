5b20897082b2f77dc91685965e5cddb0c9b23514
BytesCrafter <bytescrafter@gmail.com>
2020-05-29 23:27:15 +0800

    Added an initial design of newsfeed layout for home and profile.

--- xamarin_mobileapp/PasaBuy.App/Views/Templates/PostFeedItem.xaml.cs
+++ xamarin_mobileapp/PasaBuy.App/Views/Templates/PostFeedItem.xaml.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Essentials;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace PasaBuy.App.Views.Master
+namespace PasaBuy.App.Views.Templates
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPageDetail : ContentPage
+    public partial class PostFeedItem : ContentPage
     {
-        public MainPageDetail()
+        public PostFeedItem()
         {
             InitializeComponent();
-            //Demo.Source = "https://www.facebook.com";
         }
     }
 }
