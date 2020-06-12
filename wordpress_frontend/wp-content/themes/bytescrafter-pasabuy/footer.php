8083b1192956e928ebeb0102ca1c38e7cb51d7ba
BytesCrafter <bytescrafter@gmail.com>
2020-06-12 08:52:43 +0800

    Make a WordPress frontend theme but need official content.

---
+++ wordpress_frontend/wp-content/themes/bytescrafter-pasabuy/footer.php
@@ -0,0 +1,86 @@
+<?php
+	/**
+	* The template for displaying the header
+	*
+	* @package pasabuy
+	* @since 0.1.0
+	*/
+
+?>
+        <!-- Footer -->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="footer-col">
+                            <h4>About PasaBuy App</h4>
+                            <p>We're passionate about offering some of the best business growth services for startups.</p>
+                        </div>
+                    </div> <!-- end of col -->
+                    <div class="col-md-4">
+                        <div class="footer-col middle">
+                            <h4>Important Links</h4>
+                            <ul class="list-unstyled li-space-lg">
+                                <li class="media">
+                                    <i class="fas fa-square"></i>
+                                    <div class="media-body">Our business partners <a class="turquoise" href="#your-link">Marketplace</a></div>
+                                </li>
+                                <li class="media">
+                                    <i class="fas fa-square"></i>
+                                    <div class="media-body">Read our <a class="turquoise" href="<?php echo site_url() . '/privacy-policy'; ?>">Terms & Conditions</a>, <a class="turquoise" href="<?php echo site_url() . '/terms-and-conditions'; ?>">Privacy Policy</a></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div> <!-- end of col -->
+                    <div class="col-md-4">
+                        <div class="footer-col last">
+                            <h4>Social Media</h4>
+                            <span class="fa-stack">
+                                <a href="#your-link">
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-facebook-f fa-stack-1x"></i>
+                                </a>
+                            </span>
+                            <span class="fa-stack">
+                                <a href="#your-link">
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-twitter fa-stack-1x"></i>
+                                </a>
+                            </span>
+                            <span class="fa-stack">
+                                <a href="#your-link">
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-google-plus-g fa-stack-1x"></i>
+                                </a>
+                            </span>
+                            <span class="fa-stack">
+                                <a href="#your-link">
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-instagram fa-stack-1x"></i>
+                                </a>
+                            </span>
+                            <span class="fa-stack">
+                                <a href="#your-link">
+                                    <i class="fas fa-circle fa-stack-2x"></i>
+                                    <i class="fab fa-linkedin-in fa-stack-1x"></i>
+                                </a>
+                            </span>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright -->
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="p-small">Copyright © 2020 <a href="https://pasabuy.app">PasaBuy Tech</a> - All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ wordpress_frontend/wp-content/themes/bytescrafter-pasabuy/functions.php
@@ -0,0 +1,73 @@
+<?php
+	/**
+	 * Basically, all the logic happens here.
+	 *
+	 * @package pasabuy
+	 * @since 0.1.0
+	 */
+
+     #region WP Recommendation - Prevent direct initilization of the plugin.
+        if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
+        if ( ! function_exists( 'is_plugin_active' ) ) 
+        {
+            require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+        }
+    #endregion
+?>
+
+<?php
+
+    //Include scripts that is needed js and css.
+    function ps_plugin_frontend_enqueue()
+    {    
+        wp_enqueue_style('ps_google_fonts', 
+            'https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext',
+            false
+        );
+
+        wp_enqueue_style('ps_bootstrap_css', 
+            get_template_directory_uri() . '/assets/css/bootstrap.css', 
+            array(), 
+            false
+        );
+
+        wp_enqueue_style('ps_fontawesome_css', 
+            get_template_directory_uri() . '/assets/css/fontawesome-all.css', 
+            array(), 
+            false
+        );
+
+        wp_enqueue_style('ps_swiper_css', 
+            get_template_directory_uri() . '/assets/css/swiper.css', 
+            array(), 
+            false
+        );
+
+        wp_enqueue_style('ps_magnific_css', 
+            get_template_directory_uri() . '/assets/css/magnific-popup.css', 
+            array(), 
+            false
+        );
+
+        wp_enqueue_script('ps_jquery_js', get_template_directory_uri() . '/assets/js/jquery.min.js', array(),'3.3.1', false);
+        wp_enqueue_script('ps_bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(),'4.3.1', false);
+        wp_enqueue_script('ps_popper_js', get_template_directory_uri() . '/assets/js/popper.min.js', array(),'1.0.0', false);
+        wp_enqueue_script('ps_jquery-easing_js', get_template_directory_uri() . '/assets/js/jquery.easing.min.js', array(),'1.3.0', false);
+        wp_enqueue_script('ps_swiper_js', get_template_directory_uri() . '/assets/js/swiper.min.js', array(),'4.4.6', false);
+        wp_enqueue_script('ps_jquery-magnific-popup_js', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.js', array(),'1.1.0', false);
+        wp_enqueue_script('ps_validator_js', get_template_directory_uri() . '/assets/js/validator.min.js', array(),'0.11.8', false);
+        wp_enqueue_script('ps_scripts_js', get_template_directory_uri() . '/assets/js/scripts.js', array(),'1.0.0', false);
+
+        wp_enqueue_style( "ps_styles_css", get_stylesheet_uri() );
+    }
+    add_action( 'wp_enqueue_scripts', 'ps_plugin_frontend_enqueue' );
+
+    function my_filter_head() {
+        // show admin bar only for admins and editors. 
+        // if admin only, use: manage_options
+        if (!current_user_can('edit_posts')) {
+            add_filter('show_admin_bar', '__return_false');
+            remove_action('wp_head', '_admin_bar_bump_cb');
+        }
+    } add_action('get_header', 'my_filter_head');
+    
