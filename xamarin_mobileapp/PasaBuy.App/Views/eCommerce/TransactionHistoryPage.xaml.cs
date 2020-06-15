033ffce390bca4c4479bcc63cc5c78d3fd676696
BytesCrafter <bytescrafter@gmail.com>
2020-06-16 07:25:30 +0800

    Added transaction list view template to asset.

---
+++ xamarin_mobileapp/PasaBuy.App/Views/eCommerce/TransactionHistoryPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace PasaBuy.App.Views.eCommerce
+{
+    /// <summary>
+    /// Page to show the transaction history.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TransactionHistoryPage : ContentPage
+    {
+        public TransactionHistoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
