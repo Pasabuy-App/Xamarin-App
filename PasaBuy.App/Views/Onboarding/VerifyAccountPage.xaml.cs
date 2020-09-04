010a5b8bd450d647638096b9fe34598468f57b67
Lorz Becislao <lorzbecislao@gmail.com>
2020-09-04 18:06:03 +0800

    Added activation page UI

---
+++ PasaBuy.App/Views/Onboarding/VerifyAccountPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PasaBuy.App.Views.Onboarding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerifyAccountPage : ContentPage
+    {
+        public VerifyAccountPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
