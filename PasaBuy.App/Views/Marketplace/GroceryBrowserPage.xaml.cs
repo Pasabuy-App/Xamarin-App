2c7936d50f59a967f70aad828da7b796798d88c7
Lorz Becislao <lorzbecislao@gmail.com>
2020-09-29 22:36:37 -0700

    Added new layout for grocery tabs

--- PasaBuy.App/Views/Marketplace/GroceryBrowserPage.xaml.cs
+++ PasaBuy.App/Views/Marketplace/GroceryBrowserPage.xaml.cs
@@ -24,100 +24,12 @@ namespace PasaBuy.App.Views.Marketplace
             //this.BindingContext = StoreDataService.Instance.RestaurantViewModel;
         }
 
-        protected override void OnSizeAllocated(double width, double height)
-        {
-            base.OnSizeAllocated(width, height);
 
-            if (width > height)
-            {
-                if (Search.IsVisible)
-                {
-                    Search.WidthRequest = width;
-                }
-            }
-        }
+    
 
-        private void SearchButton_Clicked(object sender, EventArgs e)
-        {
-            this.Search.IsVisible = true;
-            this.Title.IsVisible = false;
-            this.SearchButton.IsVisible = false;
+   
 
-            if (this.TitleView != null)
-            {
-                double opacity;
 
-                // Animating Width of the search box, from 0 to full width when it added to the view.
-                var expandAnimation = new Animation(
-                    property =>
-                    {
-                        Search.WidthRequest = property;
-                        opacity = property / TitleView.Width;
-                        Search.Opacity = opacity;
-                    }, 0, TitleView.Width, Easing.Linear);
-                expandAnimation.Commit(Search, "Expand", 16, 250, Easing.Linear, (p, q) => this.SearchExpandAnimationCompleted());
-            }
-        }
-
-        private void BackToTitle_Clicked(object sender, EventArgs e)
-        {
-            this.SearchButton.IsVisible = true;
-            if (this.TitleView != null)
-            {
-                double opacity;
-
-                // Animating Width of the search box, from full width to 0 before it removed from view.
-                var shrinkAnimation = new Animation(property =>
-                {
-                    Search.WidthRequest = property;
-                    opacity = property / TitleView.Width;
-                    Search.Opacity = opacity;
-                },
-                TitleView.Width, 0, Easing.Linear);
-                shrinkAnimation.Commit(Search, "Shrink", 16, 250, Easing.Linear, (p, q) => this.SearchBoxAnimationCompleted());
-            }
-
-            SearchEntry.Text = string.Empty;
-        }
-
-        private void SearchBoxAnimationCompleted()
-        {
-            this.Search.IsVisible = false;
-            this.Title.IsVisible = true;
-        }
-
-        private void SearchExpandAnimationCompleted()
-        {
-            this.SearchEntry.Focus();
-        }
-
-        private void GroceriesTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
-        {
-            var item = e.ItemData as Groceries;
-
-            //new Alert("ok", "." + item.Id + ".HAHAHA", "ok");
-            /*StoreDetailsViewModel.store_id = item.Id;
-            StoreDetailsViewModel.loadcategory(item.Id);
-           // StoreDetailsViewModel.loadstoredetails(item.Id);
-             StoreDetailsViewModel.loaddata(item.Id);
-            //StoreDetailsViewModel.loadproduct();
-            App.Current.MainPage.Navigation.PushModalAsync(new StoreDetailsPage());*/
-            StoreDetailsViewModel.loadcategory(item.Id);
-            StoreDetailsViewModel.loadstoredetails(item.Id);
-            App.Current.MainPage.Navigation.PushModalAsync(new StoreDetailsPage());
-        }
-
-        private void RestaurantList_ItemAppearing(object sender, Syncfusion.ListView.XForms.ItemAppearingEventArgs e)
-        {
-            var item = e.ItemData as Groceries;
-            if (GroceryBrowserViewModel.grocerystorelist.Last() == item && GroceryBrowserViewModel.grocerystorelist.Count() != 1)
-            {
-                if (GroceryBrowserViewModel.grocerystorelist.IndexOf(item) >= LastIndex)
-                {
-                    LastIndex += 6;
-                    GroceryBrowserViewModel.LoadGrocery(item.Id);
-                }
-            }
-        }
+     
     }
 }
