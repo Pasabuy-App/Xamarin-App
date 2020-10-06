8d4a571416ddc48de2c8a77ab479c2161d0c58e5
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-05 22:17:36 -0700

    Added indication that no address is available in choosing address

--- PasaBuy.App/ViewModels/eCommerce/ChangeAddressViewModel.cs
+++ PasaBuy.App/ViewModels/eCommerce/ChangeAddressViewModel.cs
@@ -17,7 +17,8 @@ namespace PasaBuy.App.ViewModels.eCommerce
         public static ObservableCollection<AddressData> _addressList;
 
         private DelegateCommand _selectAddressCommand;
-        
+        private DelegateCommand _pinAnotherCommand;
+        private DelegateCommand _confirmAddress;
         public ChangeAddressViewModel()
         {
             _addressList = new ObservableCollection<AddressData>();
@@ -98,6 +99,25 @@ namespace PasaBuy.App.ViewModels.eCommerce
         public DelegateCommand SelectAddressCommand =>
             _selectAddressCommand ?? (_selectAddressCommand = new DelegateCommand(SelectAddressClicked));
 
+        public DelegateCommand PinAnotherCommand =>
+           _pinAnotherCommand ?? (_pinAnotherCommand = new DelegateCommand(PinAnotherClicked));
+
+        public DelegateCommand ConfirmAddress =>
+            _confirmAddress ?? (_confirmAddress = new DelegateCommand(ConfirmAddressClicked));
+
+        private async void ConfirmAddressClicked(object obj)
+        {
+            int numModals = Application.Current.MainPage.Navigation.ModalStack.Count;
+            for (int currModal = 0; currModal < numModals - 3; currModal++)
+            {
+                await Application.Current.MainPage.Navigation.PopModalAsync(false);
+            }
+        }
+        private async void PinAnotherClicked(object obj)
+        {
+            await Application.Current.MainPage.Navigation.PushModalAsync(new AddressInMapPage());
+
+        }
         private void SelectAddressClicked(object obj)
         {
             try
