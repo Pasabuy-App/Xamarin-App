aeb1b9ccd6923db0e574da17e717ae76361020ef
C# Developer <C# Developer@DESKTOP-UFSDQBI>
2020-10-06 12:09:36 +0800

    Data integrate the list for Selecting Address in Checkout.

--- PasaBuy.App/ViewModels/eCommerce/ChangeAddressViewModel.cs
+++ PasaBuy.App/ViewModels/eCommerce/ChangeAddressViewModel.cs
@@ -1,4 +1,7 @@
-﻿using PasaBuy.App.Commands;
+﻿using Newtonsoft.Json;
+using PasaBuy.App.Commands;
+using PasaBuy.App.Controllers.Notice;
+using PasaBuy.App.Local;
 using PasaBuy.App.Models.Settings;
 using PasaBuy.App.Views.eCommerce;
 using System;
@@ -11,15 +14,16 @@ namespace PasaBuy.App.ViewModels.eCommerce
 {
     public class ChangeAddressViewModel: BaseViewModel
     {
-        private ObservableCollection<AddressData> _addressList;
+        public static ObservableCollection<AddressData> _addressList;
 
         private DelegateCommand _selectAddressCommand;
         
         public ChangeAddressViewModel()
         {
             _addressList = new ObservableCollection<AddressData>();
-
-            for (int i = 0; i < 4; i++)
+            _addressList.Clear();
+            LoadData();
+           /* for (int i = 0; i < 4; i++)
             {
                 _addressList.Add(new AddressData
                 {
@@ -32,10 +36,52 @@ namespace PasaBuy.App.ViewModels.eCommerce
                     ContactNumber = "09385956099",
                     ContactPerson = "Lorz Becislao"
                 });
-            }
+            }*/
            
         }
+        public static void LoadData()
+        {
+            try
+            {
+                DataVice.Address.Instance.List(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        AddressData address = JsonConvert.DeserializeObject<AddressData>(data);
 
+                        for (int i = 0; i < address.data.Length; i++)
+                        {
+                            string types = address.data[i].types;
+                            string status = address.data[i].status;
+                            string type = string.Empty;
+                            if (types == "home") { type = "Home"; }
+                            if (types == "office") { type = "Office"; }
+                            if (types == "business") { type = "Business"; }
+                            _addressList.Add(new AddressData
+                            {
+                                ID = address.data[i].id,
+                                Types = types,
+                                Country = address.data[i].country,
+                                City = address.data[i].city,
+                                Province = address.data[i].province,
+                                FullAddress = address.data[i].street + " " + address.data[i].brgy + " " + address.data[i].city + " " + address.data[i].province + ", " + address.data[i].country,
+                                ContactNumber = address.data[i].contact,
+                                ContactPerson = address.data[i].contact_person
+                            });
+                        }
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
         public ObservableCollection<AddressData> AddressList
         {
             get
@@ -52,9 +98,41 @@ namespace PasaBuy.App.ViewModels.eCommerce
         public DelegateCommand SelectAddressCommand =>
             _selectAddressCommand ?? (_selectAddressCommand = new DelegateCommand(SelectAddressClicked));
 
-        private async void SelectAddressClicked(object obj)
+        private void SelectAddressClicked(object obj)
         {
-            await Application.Current.MainPage.Navigation.PushModalAsync(new AddressInMapPage());
+            try
+            {
+                var btn = obj as TapGestureRecognizer;
+                //Console.WriteLine("ClassID: " + btn.ClassId);
+                DataVice.Address.Instance.SelectByID(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, btn.ClassId, (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        AddressData address = JsonConvert.DeserializeObject<AddressData>(data);
+                        AddressInMapPage.street = btn.ClassId;
+                        for (int i = 0; i < address.data.Length; i++)
+                        {
+                            AddressInMapPage.street = address.data[i].street;
+                            AddressInMapPage.lat = Convert.ToDouble(address.data[i].latitude);
+                            AddressInMapPage.lon = Convert.ToDouble(address.data[i].longitude);
+                        }
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            await Application.Current.MainPage.Navigation.PushModalAsync(new AddressInMapPage());
+                        });
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+
+                    }
+                });
+                //await Application.Current.MainPage.Navigation.PushModalAsync(new AddressInMapPage());
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
         }
 
     }
--- PasaBuy.App/Views/eCommerce/AddressInMapPage.xaml.cs
+++ PasaBuy.App/Views/eCommerce/AddressInMapPage.xaml.cs
@@ -12,9 +12,14 @@ namespace PasaBuy.App.Views.eCommerce
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddressInMapPage : ContentPage
     {
+        public static string street;
+        public static string address_id;
+        public static double lat;
+        public static double lon;
         public AddressInMapPage()
         {
             InitializeComponent();
+            StreetEntry.Text = street + " lan: " + lat.ToString() + " lon: " + lon.ToString() + " id: " + address_id;
         }
     }
 }
