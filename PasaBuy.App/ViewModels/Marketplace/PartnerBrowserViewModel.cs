50f99ae0ccbd86cb43592b8b14c8f02799f68c60
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-07 23:09:31 -0700

    Added layout for partner browser page

--- PasaBuy.App/ViewModels/Marketplace/PartnerBrowserViewModel.cs
+++ PasaBuy.App/ViewModels/Marketplace/PartnerBrowserViewModel.cs
@@ -2,10 +2,12 @@
 using PasaBuy.App.Controllers.Notice;
 using PasaBuy.App.Local;
 using PasaBuy.App.Models.Marketplace;
+using PasaBuy.App.Views.Notice;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace PasaBuy.App.ViewModels.Marketplace
 {
@@ -13,7 +15,22 @@ namespace PasaBuy.App.ViewModels.Marketplace
     {
         public  ObservableCollection<PartnerStore> partnerStoresRotator;
 
+        public static ObservableCollection<Store> storeList;
+
         public static ObservableCollection<Categories> itemCategories;
+
+        public ObservableCollection<Store> StoreList
+        {
+            get
+            {
+                return storeList;
+            }
+            set
+            {
+                storeList = value;
+                this.NotifyPropertyChanged();
+            }
+        }
 
         public ObservableCollection<Categories> ItemCategories
         {
@@ -42,7 +59,9 @@ namespace PasaBuy.App.ViewModels.Marketplace
 
         public PartnerBrowserViewModel()
         {
+            storeList = new ObservableCollection<Store>();
             itemCategories = new ObservableCollection<Categories>();
+            storeList.Clear();
             itemCategories.Clear();
 
             this.PartnerStoresRotator = new ObservableCollection<PartnerStore>()
@@ -89,6 +108,50 @@ namespace PasaBuy.App.ViewModels.Marketplace
                     }
                 });
 
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
+
+        public static void LoadStore(string catid, string lastid)
+        {
+            try
+            {
+                TindaPress.Store.Instance.List(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, catid, "", "1", lastid, (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        StoreListData datas = JsonConvert.DeserializeObject<StoreListData>(data);
+                        Console.WriteLine(data);
+                        if (datas.data.Length == 0)
+                        {
+                            (App.Current.MainPage).Navigation.PushModalAsync(new NavigationPage(new NoStoresPage()));
+
+                        }
+                        else
+                        {
+                            for (int i = 0; i < datas.data.Length; i++)
+                            {
+                                storeList.Add(new Store()
+                                {
+                                    Id = datas.data[i].ID,
+                                    Title = datas.data[i].title,
+                                    Description = datas.data[i].short_info,
+                                    Logo = datas.data[i].avatar == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-store.png" : PSAProc.GetUrl(datas.data[i].avatar),
+                                    Offer = "50% off",
+                                    ItemRating = "4.5",
+                                    Banner = datas.data[i].banner == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-banner.png" : PSAProc.GetUrl(datas.data[i].banner)
+                                });
+                            }
+                        }
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                    }
+                });
             }
             catch (Exception e)
             {
--- PasaBuy.App/Views/Marketplace/PartnerBrowserPage.xaml.cs
+++ PasaBuy.App/Views/Marketplace/PartnerBrowserPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PasaBuy.App.ViewModels.Marketplace;
+﻿using PasaBuy.App.Models.Marketplace;
+using PasaBuy.App.ViewModels.Marketplace;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,26 @@ namespace PasaBuy.App.Views.Marketplace
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PartnerBrowserPage : ContentView
     {
+        public static bool isTapped = false;
         public PartnerBrowserPage()
         {
             InitializeComponent();
             this.BindingContext = new PartnerBrowserViewModel();
             PartnerBrowserViewModel.LoadCategory();
+            Loading.IsRunning = false;
+            Loading.IsVisible = false;
+        }
+
+        private async void Partner_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        {
+            Loading.IsRunning = true;
+            Loading.IsVisible = true;
+            var item = e.ItemData as Categories;
+            StoreBrowserViewModel.LoadStore(item.Id, "");
+            await App.Current.MainPage.Navigation.PushModalAsync(new PartnerListPage());
+            Loading.IsRunning = false;
+            Loading.IsVisible = false;
+
         }
     }
 }
