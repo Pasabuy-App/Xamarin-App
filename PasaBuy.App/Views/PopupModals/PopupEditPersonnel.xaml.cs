3f3a1ad1c70b9a66bd267d4aecb423396c79f6f7
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-11 02:13:38 -0700

    Added layout on adding new personnel

---
+++ PasaBuy.App/Views/PopupModals/PopupEditPersonnel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PasaBuy.App.Views.PopupModals
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopupEditPersonnel : ContentView
+    {
+        public PopupEditPersonnel()
+        {
+            InitializeComponent();
+        }
+    }
+}
