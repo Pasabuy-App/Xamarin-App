6433cc3673a72428ef2e1d00861126fbfefc534b
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-12 09:33:47 -0700

    Added listing of variant options

---
+++ PasaBuy.App/Views/StoreViews/Management/OptionsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PasaBuy.App.ViewModels.MobilePOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PasaBuy.App.Views.StoreViews.Management
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OptionsView : ContentPage
+    {
+        public OptionsView()
+        {
+            InitializeComponent();
+            this.BindingContext = new VariantsViewModel();
+        }
+
+        public void BackButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
