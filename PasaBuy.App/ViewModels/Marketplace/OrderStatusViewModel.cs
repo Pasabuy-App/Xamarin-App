ffd10d9339bc9ebcc3ac98743760baeabe9635d4
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-14 13:05:22 -0700

    Added orderstage page initial UI

---
+++ PasaBuy.App/ViewModels/Marketplace/OrderStatusViewModel.cs
@@ -0,0 +1,49 @@
+﻿using FFImageLoading.Transformations;
+using FFImageLoading.Work;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasaBuy.App.ViewModels.Marketplace
+{
+    public class OrderStatusViewModel : BaseViewModel
+    {
+        public bool IsCurrentStatus { get; set; }
+
+        public List<ITransformation> TurnGreyScale { get; set; }
+
+        public OrderStatusViewModel()
+        {
+            TurnGreyScale = new List<ITransformation>()
+            {
+                new CustomTransformationSelector(),
+            };
+
+        }
+
+      
+        public class CustomTransformationSelector : ITransformation
+        {
+            readonly ITransformation PlaceholderTransformation = new CircleTransformation(5d, "#EEEEEE");
+            readonly ITransformation ImageTransformation = new GrayscaleTransformation();
+
+            public string Key
+            {
+                get
+                {
+                    return "CustomTransformationSelector";
+                }
+            }
+
+            public IBitmap Transform(IBitmap sourceBitmap, string path, ImageSource source, bool isPlaceholder, string key)
+            {
+                if (isPlaceholder)
+                {
+                    return PlaceholderTransformation.Transform(sourceBitmap, path, source, isPlaceholder, key);
+                }
+
+                return ImageTransformation.Transform(sourceBitmap, path, source, isPlaceholder, key);
+            }
+        }
+    }
+}
