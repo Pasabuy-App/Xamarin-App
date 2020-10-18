8f5eb14f701e072bf6938991e1b54afc6c70d744
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-18 22:19:42 +0800

    Data integrate Verify User in Maintabs for PopupVerify and Settings Verify Account.

---
+++ PasaBuy.App/Http/DataFeature.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using PasaBuy.App.Local;
+using PasaBuy.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasaBuy.App.Http
+{
+    public class DataFeature
+    {
+        #region Static
+        private static DataFeature instance;
+        public static DataFeature Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataFeature();
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public DataFeature()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+
+        #region VerifyUser Method
+        public async void VerifyUser(Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+                dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
+                dict.Add("snky", PSACache.Instance.UserInfo.snky);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/datavice/v1/user/verify/docs", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
