b238e81d3c4fca382f9de25028d36b98d6ce9f63
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-20 15:22:00 +0800

    Added Search in StoreList Page.'

--- PasaBuy.App/ViewModels/Marketplace/StoreListViewModel.cs
+++ PasaBuy.App/ViewModels/Marketplace/StoreListViewModel.cs
@@ -138,5 +138,49 @@ namespace PasaBuy.App.ViewModels.Marketplace
                 new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
             }
         }
+
+        public static void SearchStore(string search)
+        {
+            try
+            {
+                TindaPress.Store.Instance.Search(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, search, (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        GroceriesStoreListData datas = JsonConvert.DeserializeObject<GroceriesStoreListData>(data);
+                        if (datas.data.Length > 0)
+                        {
+                            storeList.Clear();
+                            for (int i = 0; i < datas.data.Length; i++)
+                            {
+                                storeList.Add(new Store()
+                                {
+                                    Id = datas.data[i].ID,
+                                    Title = datas.data[i].title,
+                                    Description = datas.data[i].short_info,
+                                    Logo = datas.data[i].avatar == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-store.png" : PSAProc.GetUrl(datas.data[i].avatar),
+                                    Offer = "50% off",
+                                    ItemRating = "4.5",
+                                    Banner = datas.data[i].banner == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-banner.png" : PSAProc.GetUrl(datas.data[i].banner),
+                                    Street = datas.data[i].brgy + " " + datas.data[i].city
+                                });
+                            }
+                        }
+                        else
+                        {
+                            new Alert("Notice to User", "No store found.", "Try Again");
+                        }
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
     }
 }
--- PasaBuy.App/Views/Marketplace/StoreListPage.xaml.cs
+++ PasaBuy.App/Views/Marketplace/StoreListPage.xaml.cs
@@ -21,6 +21,42 @@ namespace PasaBuy.App.Views.Marketplace
             this.BindingContext = new StoreListViewModel();
             PageTitle.Text = pageTitle;
             isTapped = false;
+            SearchEntry.Completed += (sender, args) => SearchStore(sender, args);
+            StoreListViewModel.storeList.CollectionChanged += CollectionChanges;
+        }
+        private void CollectionChanges(object sender, EventArgs e)
+        {
+            this.SearchButton.IsVisible = true;
+            if (this.TitleView != null)
+            {
+                double opacity;
+
+                // Animating Width of the search box, from full width to 0 before it removed from view.
+                var shrinkAnimation = new Animation(property =>
+                {
+                    Search.WidthRequest = property;
+                    opacity = property / TitleView.Width;
+                    Search.Opacity = opacity;
+                },
+                TitleView.Width, 0, Easing.Linear);
+                shrinkAnimation.Commit(Search, "Shrink", 16, 250, Easing.Linear, (p, q) => this.SearchBoxAnimationCompleted());
+            }
+
+            SearchEntry.Text = string.Empty;
+        }
+        public void SearchStore(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(SearchEntry.Text))
+                {
+                    StoreListViewModel.SearchStore(SearchEntry.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                new Controllers.Notice.Alert("Something went Wrong", "Please contact administrator. Error: " + ex, "OK");
+            }
         }
 
         private async void StoreTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
