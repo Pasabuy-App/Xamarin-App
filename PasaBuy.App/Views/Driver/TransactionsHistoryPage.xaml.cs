bfe704464dad44d6705fa03e795a3f4ad25d4503
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-20 19:09:00 +0800

    Data integrate My Driver > Transaction no rest but with example data. Listing, 12-7 and pull to refresh.

--- PasaBuy.App/Views/Driver/TransactionsHistoryPage.xaml.cs
+++ PasaBuy.App/Views/Driver/TransactionsHistoryPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,11 +13,32 @@ namespace PasaBuy.App.Views.Driver
         public TransactionsHistoryPage()
         {
             InitializeComponent();
+            this.BindingContext = new ViewModels.Driver.TransactionHistoryViewModel();
+            pullToRefresh.Refreshing += PullToRefresh_Refreshing;
         }
 
-        private void NewOrders_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        private async void PullToRefresh_Refreshing(object sender, EventArgs args)
         {
+            pullToRefresh.IsRefreshing = true;
+            await Task.Delay(500);
+            ViewModels.Driver.TransactionHistoryViewModel.LastIndex = 11;
+            ViewModels.Driver.TransactionHistoryViewModel._HistoryList.Clear();
+            ViewModels.Driver.TransactionHistoryViewModel.LoadData("");
+            pullToRefresh.IsRefreshing = false;
+        }
 
+        private void NewOrders_ItemAppearing(object sender, Syncfusion.ListView.XForms.ItemAppearingEventArgs e)
+        {
+            var item = e.ItemData as Models.Driver.TransactListData;
+            if (ViewModels.Driver.TransactionHistoryViewModel._HistoryList.Last() == item && ViewModels.Driver.TransactionHistoryViewModel._HistoryList.Count() != 1)
+            {
+                if (ViewModels.Driver.TransactionHistoryViewModel._HistoryList.IndexOf(item) >= ViewModels.Driver.TransactionHistoryViewModel.LastIndex)
+                {
+                    ViewModels.Driver.TransactionHistoryViewModel.LastIndex += 6;
+                    new Controllers.Notice.Alert("There is no ViewDetailsPage", "This is the id of Transaction : " + item.ID, "OK");
+                    //ViewModels.Driver.TransactionHistoryViewModel._HistoryList.LoadData(item.Last_ID);
+                }
+            }
         }
     }
 }
