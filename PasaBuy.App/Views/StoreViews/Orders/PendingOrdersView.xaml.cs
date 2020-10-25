98b0d115282c0260e0fd29fd17550d7d6e6b9b18
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-25 13:45:27 +0800

    Fixed bugs in Pending Order in Store Dashboard.

--- PasaBuy.App/Views/StoreViews/Orders/PendingOrdersView.xaml.cs
+++ PasaBuy.App/Views/StoreViews/Orders/PendingOrdersView.xaml.cs
@@ -9,27 +9,37 @@ namespace PasaBuy.App.Views.StoreViews.Orders
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PendingOrdersView : ContentView
     {
+        public bool isTapped;
         public PendingOrdersView()
         {
             InitializeComponent();
+            isTapped = false;
             /*this.BindingContext = new DashboardOrdersViewModel();
             DashboardOrdersViewModel.orderList.Clear();
             DashboardOrdersViewModel.LoadOrder("received", "");*/
         }
 
-        private void PendingOrders_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        private async void PendingOrders_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
         {
-            var item = e.ItemData as OrdersDataModel;
-            TransactionDetailsView.id = item.ID;
-            TransactionDetailsView.customer = item.Customer;
-            TransactionDetailsView.orderid = item.OrderID;
-            TransactionDetailsView.totalprice = item.TotalPrice;
-            TransactionDetailsView.datecreated = item.Date_Time;
-            TransactionDetailsView.method = item.Method;
-            OrderDetailsViewModel.LoadOrder(item.Stage, item.ID);
-            TransactionDetailsView.order_type = "Received";
-            TransactionDetailsView.stage_type = "received";
-            Navigation.PushModalAsync(new TransactionDetailsView());
+            if (!isTapped)
+            {
+                isTapped = true;
+                var item = e.ItemData as OrdersDataModel;
+                TransactionDetailsView.id = item.ID;
+                TransactionDetailsView.avatar = item.Avatar;
+                TransactionDetailsView.user_id = item.User_ID;
+                TransactionDetailsView.customer = item.Customer;
+                TransactionDetailsView.orderid = item.OrderID;
+                TransactionDetailsView.totalprice = item.TotalPrice;
+                TransactionDetailsView.datecreated = item.Date_Time;
+                TransactionDetailsView.method = item.Method;
+                OrderDetailsViewModel.LoadOrder(item.Stage, Local.PSACache.Instance.UserInfo.stid, item.ID);
+                TransactionDetailsView.order_type = "Received";
+                TransactionDetailsView.stage_type = "received";
+                await Navigation.PushModalAsync(new TransactionDetailsView());
+                await System.Threading.Tasks.Task.Delay(500);
+                isTapped = false;
+            }
         }
     }
 }
