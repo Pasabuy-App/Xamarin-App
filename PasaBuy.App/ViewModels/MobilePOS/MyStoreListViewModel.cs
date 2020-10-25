6aed9a6981f48cfa77a0a49dbf9c66af00b433d9
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-25 13:40:51 +0800

    Added pull to refresh in MyStoreListPage.

--- PasaBuy.App/ViewModels/MobilePOS/MyStoreListViewModel.cs
+++ PasaBuy.App/ViewModels/MobilePOS/MyStoreListViewModel.cs
@@ -4,6 +4,9 @@ using PasaBuy.App.Local;
 using PasaBuy.App.Models.Marketplace;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace PasaBuy.App.ViewModels.MobilePOS
 {
@@ -23,74 +26,119 @@ namespace PasaBuy.App.ViewModels.MobilePOS
                 this.NotifyPropertyChanged();
             }
         }
+        bool _IsRunning = false;
+        public bool IsRunning
+        {
+            get
+            {
+                return _IsRunning;
+            }
+            set
+            {
+                if (_IsRunning != value)
+                {
+                    _IsRunning = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get
+            {
+                return _isRefreshing;
+            }
+            set
+            {
+                if (_isRefreshing != value)
+                {
+                    _isRefreshing = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public ICommand RefreshCommand { protected set; get; }
+
 
         public MyStoreListViewModel()
         {
             myStores = new ObservableCollection<Store>();
             LoadStore();
+            RefreshCommand = new Command<string>((key) =>
+            {
+                myStores.Clear();
+                LoadStore();
+                IsRefreshing = false;
+            });
         }
         public void LoadStore()
         {
             try
             {
-                TindaPress.Personnel.Instance.Store_List(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, "active", (bool success, string data) =>
+                if (!IsRunning)
                 {
-                    if (success)
+                    IsRunning = true;
+                    TindaPress.Personnel.Instance.Store_List(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, "active", (bool success, string data) =>
                     {
-                        StoreListData datas = JsonConvert.DeserializeObject<StoreListData>(data);
-
-                        for (int i = 0; i < datas.data.Length; i++)
+                        if (success)
                         {
-                            myStores.Add(new Store()
-                            {
-                                Id = datas.data[i].ID,
-                                //RoleID = datas.data[i].roid,
-                                Title = datas.data[i].title,
-                                Description = datas.data[i].short_info,
-                                Logo = datas.data[i].avatar == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-store.png" : PSAProc.GetUrl(datas.data[i].avatar),
-                                Banner = datas.data[i].banner == "None" ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-banner.png" : PSAProc.GetUrl(datas.data[i].banner)
-                            });
-                        }
-                    }
-                    else
-                    {
-                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
-                    }
-                });
-                /*myStores.Add(new Store()
-                {
-                    Title = "My First Store",
-                    Description = "First Description",
-                    Logo = "https://pasabuy.app/wp-content/uploads/2020/10/Food-Template.jpg"
-                });*/
+                            StoreListData datas = JsonConvert.DeserializeObject<StoreListData>(data);
 
+                            for (int i = 0; i < datas.data.Length; i++)
+                            {
+                                myStores.Add(new Store()
+                                {
+                                    Id = datas.data[i].ID,
+                                    //RoleID = datas.data[i].roid,
+                                    Title = datas.data[i].title,
+                                    Description = datas.data[i].short_info,
+                                    Logo = datas.data[i].avatar == "None" || string.IsNullOrEmpty(datas.data[i].avatar) ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-store.png" : PSAProc.GetUrl(datas.data[i].avatar),
+                                    Banner = datas.data[i].banner == "None" || string.IsNullOrEmpty(datas.data[i].banner) ? "https://pasabuy.app/wp-content/plugins/TindaPress/assets/images/default-banner.png" : PSAProc.GetUrl(datas.data[i].banner)
+                                });
+                            }
+                            IsRunning = false ;
+                        }
+                        else
+                        {
+                            new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                            IsRunning = false;
+                        }
+                    });
+                }
             }
             catch (Exception e)
             {
                 new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+                IsRunning = false;
             }
-            /*this.MyStores = new ObservableCollection<Store>()
-            {
-                new Store
-                {
-                    Title = "My First Store",
-                    Description = "First Description",
-                    Logo = "https://pasabuy.app/wp-content/uploads/2020/10/Food-Template.jpg"
+        }
 
-                },
-                new Store
-                {
-                    Title = "My Second Store",
-                    Description = "Second Description",
-                    Logo = "https://pasabuy.app/wp-content/uploads/2020/10/Food-Template.jpg"
-                },
-                new Store
-                {
-                    Title = "My Third Store",
-                    Description = "Third Description",
-                    Logo = "https://pasabuy.app/wp-content/uploads/2020/10/Food-Template.jpg"
-                },
-            };*/
+        private Command<object> itemTappedCommand;
+        public Command<object> ItemTappedCommand
+        {
+            get
+            {
+                return this.itemTappedCommand ?? (this.itemTappedCommand = new Command<object>(this.NavigateToNextPage));
+            }
+        }
+
+        private async void NavigateToNextPage(object selectedItem)
+        {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                Views.Navigation.MasterView.MyType = "store";
+                PSACache.Instance.UserInfo.stid = ((selectedItem as Syncfusion.ListView.XForms.ItemTappedEventArgs)?.ItemData as Store).Id;
+                PSACache.Instance.UserInfo.store_name = ((selectedItem as Syncfusion.ListView.XForms.ItemTappedEventArgs)?.ItemData as Store).Title;
+                //PSACache.Instance.UserInfo.roid = ((selectedItem as Syncfusion.ListView.XForms.ItemTappedEventArgs)?.ItemData as Store).roid;
+                PSACache.Instance.UserInfo.store_logo = ((selectedItem as Syncfusion.ListView.XForms.ItemTappedEventArgs)?.ItemData as Store).Logo;
+                PSACache.Instance.UserInfo.store_banner = ((selectedItem as Syncfusion.ListView.XForms.ItemTappedEventArgs)?.ItemData as Store).Banner;
+                await Task.Delay(300);
+                App.Current.MainPage = new Views.Navigation.NavigationView();
+                await Task.Delay(300);
+                IsRunning = false;
+            }
         }
     }
 }
