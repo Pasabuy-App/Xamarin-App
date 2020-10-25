498661ee74f57577df01a878164fc7fdf6795ff6
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-25 13:44:35 +0800

    Fixed bugs in Declined Order in Store Dashboard.

--- PasaBuy.App/Views/StoreViews/Orders/DeclinedOrdersView.xaml.cs
+++ PasaBuy.App/Views/StoreViews/Orders/DeclinedOrdersView.xaml.cs
@@ -9,27 +9,37 @@ namespace PasaBuy.App.Views.StoreViews.Orders
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DeclinedOrdersView : ContentView
     {
+        public bool isTapped;
         public DeclinedOrdersView()
         {
             InitializeComponent();
+            isTapped = false;
             /*this.BindingContext = new DashboardOrdersViewModel();
             DashboardOrdersViewModel.orderList.Clear();
             DashboardOrdersViewModel.LoadOrder("cancelled", "")*/
             ;
         }
 
-        private void DeclinedOrders_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        private async void DeclinedOrders_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
         {
-            var item = e.ItemData as OrdersDataModel;
-            TransactionDetailsView.id = item.ID;
-            TransactionDetailsView.customer = item.Customer;
-            TransactionDetailsView.orderid = item.OrderID;
-            TransactionDetailsView.totalprice = item.TotalPrice;
-            TransactionDetailsView.datecreated = item.Date_Time;
-            TransactionDetailsView.method = item.Method;
-            TransactionDetailsView.order_type = "Declined";
-            OrderDetailsViewModel.LoadOrder(item.Stage, item.ID);
-            Navigation.PushModalAsync(new TransactionDetailsView());
+            if (!isTapped)
+            {
+                isTapped = true;
+                var item = e.ItemData as OrdersDataModel;
+                TransactionDetailsView.id = item.ID;
+                TransactionDetailsView.avatar = item.Avatar;
+                TransactionDetailsView.user_id = item.User_ID;
+                TransactionDetailsView.customer = item.Customer;
+                TransactionDetailsView.orderid = item.OrderID;
+                TransactionDetailsView.totalprice = item.TotalPrice;
+                TransactionDetailsView.datecreated = item.Date_Time;
+                TransactionDetailsView.method = item.Method;
+                TransactionDetailsView.order_type = "Declined";
+                OrderDetailsViewModel.LoadOrder(item.Stage, Local.PSACache.Instance.UserInfo.stid, item.ID);
+                await Navigation.PushModalAsync(new TransactionDetailsView());
+                await System.Threading.Tasks.Task.Delay(500);
+                isTapped = false;
+            }
         }
     }
 }
