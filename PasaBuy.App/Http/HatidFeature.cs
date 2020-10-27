e2a0c3c05fbc4be64855ebb7333c2983dd809512
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-27 20:56:31 +0800

    Data integration of Vehicle Document Listing with pull to refresh.

--- PasaBuy.App/Http/HatidFeature.cs
+++ PasaBuy.App/Http/HatidFeature.cs
@@ -3,6 +3,7 @@ using PasaBuy.App.Local;
 using PasaBuy.App.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 
 namespace PasaBuy.App.Http
@@ -242,6 +243,67 @@ namespace PasaBuy.App.Http
             var content = new FormUrlEncodedContent(dict);
 
             var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/hatidpress/v2/mover/vehicle/document/list", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+
+        /// <summary>
+        /// Insert of documents of vehicle.
+        /// </summary>
+        public async void Insert_VDocuments(string img, string vhid, string types, string comments, Action<bool, string> callback)
+        {
+            var multiForm = new MultipartFormDataContent();
+
+                multiForm.Add(new StringContent(PSACache.Instance.UserInfo.wpid), "wpid");
+                multiForm.Add(new StringContent(PSACache.Instance.UserInfo.snky), "snky");
+                multiForm.Add(new StringContent(vhid), "vhid");
+                multiForm.Add(new StringContent(types), "types");
+                multiForm.Add(new StringContent(comments), "comments");
+                FileStream fs = File.OpenRead(img);
+                multiForm.Add(new StreamContent(fs), "img", Path.GetFileName(img));
+
+            var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/hatidpress/v2/mover/vehicle/document/insert", multiForm);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+
+        /// <summary>
+        /// Insert of documents of vehicle.
+        /// </summary>
+        public async void Listing_DocTyoe(Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+                dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
+                dict.Add("snky", PSACache.Instance.UserInfo.snky);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/hatidpress/v2/mover/vehicle/document/type/list", content);
             response.EnsureSuccessStatusCode();
 
             if (response.IsSuccessStatusCode)
--- PasaBuy.App/ViewModels/Driver/DriverDocumentsViewModel.cs
+++ PasaBuy.App/ViewModels/Driver/DriverDocumentsViewModel.cs
@@ -1,5 +1,11 @@
-﻿using PasaBuy.App.Models.Driver;
+﻿using Newtonsoft.Json;
+using PasaBuy.App.Controllers.Notice;
+using PasaBuy.App.Local;
+using PasaBuy.App.Models.Driver;
+using System;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace PasaBuy.App.ViewModels.Driver
@@ -22,30 +28,149 @@ namespace PasaBuy.App.ViewModels.Driver
                 this.NotifyPropertyChanged();
             }
         }
+        public bool isClicked = false;
+
+        bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get
+            {
+                return _isRefreshing;
+            }
+            set
+            {
+                if (_isRefreshing != value)
+                {
+                    _isRefreshing = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public ICommand RefreshCommand { protected set; get; }
         public DriverDocumentsViewModel()
         {
 
             this.AddDocumentsCommand = new Command(this.AddDocumentsClicked);
             _documentsList = new ObservableCollection<DriverDocuments>();
 
-            for (int i = 0; i < 5; i++)
+            RefreshDocument(); 
+            RefreshCommand = new Command<string>((key) =>
             {
-                _documentsList.Add(new DriverDocuments()
+                RefreshDocument();
+                IsRefreshing = false;
+            });
+        }
+        public void RefreshDocument()
+        {
+            try
+            {
+                if (!IsBusy)
                 {
-                    Name = "OR Vehicle Name",
-                    Type = "OR Vehicle",
-                    Date = "Sept. 5, 2020"
-                });
+                    IsBusy = true;
+                    _documentsList.Clear();
+                    Http.HatidFeature.Instance.Listing_VDocuments(PSACache.Instance.UserInfo.vhid, "", (bool success, string data) =>
+                    {
+                        if (success)
+                        {
+                            CultureInfo provider = new CultureInfo("fr-FR");
+                            DriverDocuments docs = JsonConvert.DeserializeObject<DriverDocuments>(data);
+                            for (int i = 0; i < docs.data.Length; i++)
+                            {
+                                DateTime date = DateTime.ParseExact(docs.data[i].date_created, "yyyy-MM-dd HH:mm:ss", provider);
+                                bool isGreen;
+                                bool isRed;
+                                if (docs.data[i].status != "Approve")
+                                {
+                                    isGreen = true;
+                                    isRed = false;
+                                }
+                                else
+                                {
+                                    isGreen = false;
+                                    isRed = true;
+                                }
+                                _documentsList.Add(new DriverDocuments()
+                                {
+                                    Type = docs.data[i].types,
+                                    Status = docs.data[i].status,
+                                    isGreen = isGreen,
+                                    isRed = isRed,
+                                    Date = date.ToString("MMM. dd, yyyy")
+                                });
+                            }
+                            IsBusy = false;
+                        }
+                        else
+                        {
+                            new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                            IsBusy = false;
+                        }
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+                IsBusy = false;
             }
         }
 
-        private void AddDocumentsClicked(object obj)
+        public static void LoadDocument()
         {
-            Device.BeginInvokeOnMainThread(async () =>
+            try
             {
-                await App.Current.MainPage.Navigation.PushModalAsync(new Views.Driver.AddDocumentPage());
-            });
+                _documentsList.Clear();
+                Http.HatidFeature.Instance.Listing_VDocuments(PSACache.Instance.UserInfo.vhid, "", (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        CultureInfo provider = new CultureInfo("fr-FR");
+                        DriverDocuments docs = JsonConvert.DeserializeObject<DriverDocuments>(data);
+                        for (int i = 0; i < docs.data.Length; i++)
+                        {
+                            DateTime date = DateTime.ParseExact(docs.data[i].date_created, "yyyy-MM-dd HH:mm:ss", provider);
+                            bool isGreen;
+                            bool isRed;
+                            if (docs.data[i].status != "Approve")
+                            {
+                                isGreen = true;
+                                isRed = false;
+                            }
+                            else
+                            {
+                                isGreen = false;
+                                isRed = true;
+                            }
+                            _documentsList.Add(new DriverDocuments()
+                            {
+                                Type = docs.data[i].types,
+                                Status = docs.data[i].status,
+                                isGreen = isGreen,
+                                isRed = isRed,
+                                Date = date.ToString("MMM. dd, yyyy")
+                            });
+                        }
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
 
+        private async void AddDocumentsClicked(object obj)
+        {
+            if (!isClicked)
+            {
+                isClicked = true;
+                await App.Current.MainPage.Navigation.PushModalAsync(new Views.Driver.AddDocumentPage());
+                isClicked = false;
+            }
         }
     }
 }
