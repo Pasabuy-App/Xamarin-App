952c5356b22d34c5b5bc2dfa67a6bd381bc9f1b8
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-27 15:52:22 +0800

    Data integration of inserting vehicle.

---
+++ PasaBuy.App/Models/Driver/MoverDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasaBuy.App.Models.Driver
+{
+    public class MoverDataModel
+    {
+        public MoverData[] data;
+    }
+    public class MoverData
+    {
+        public string mvid = string.Empty;
+        public string date_created = string.Empty;
+        public string mover_doc = string.Empty;
+        public int rate;
+    }
+}
---
+++ PasaBuy.App/ViewModels/Driver/MakerListViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using PasaBuy.App.Controllers.Notice;
+using PasaBuy.App.Local;
+using PasaBuy.App.Models.Driver;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PasaBuy.App.ViewModels.Driver
+{
+    public class MakerListViewModel
+    {
+        public ObservableCollection<TypeListModel> _MakerList;
+
+        public ObservableCollection<TypeListModel> MakerList
+        {
+            get
+            {
+                return _MakerList;
+            }
+            set
+            {
+                _MakerList = value;
+            }
+        }
+        public MakerListViewModel(string val)
+        {
+            try
+            {
+                _MakerList = new ObservableCollection<TypeListModel>();
+                Http.HatidFeature.Instance.Listing_Maker(val, "", "", (bool success, string data) =>
+                {
+                    if (success)
+                    {
+                        TypeListModel type = JsonConvert.DeserializeObject<TypeListModel>(data);
+                        for (int i = 0; i < type.data.Length; i++)
+                        {
+                            _MakerList.Add(new TypeListModel()
+                            {
+                                ID = type.data[i].ID,
+                                Title = type.data[i].title
+                            });
+                        }
+                        Console.WriteLine("data: " + data);
+                    }
+                    else
+                    {
+                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
+    }
+}
