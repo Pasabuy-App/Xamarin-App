ea49164d782a17d062f4e8adeb8f6fd95405c2a8
Lorz Becislao <lorzbecislao@gmail.com>
2020-10-29 19:58:17 -0700

    Added searchbar in pasabuyplus list of stores

--- PasaBuy.App/Views/Currency/PasabuyStoreList.xaml.cs
+++ PasaBuy.App/Views/Currency/PasabuyStoreList.xaml.cs
@@ -23,5 +23,72 @@ namespace PasaBuy.App.Views.Currency
         {
             await Navigation.PopModalAsync();
         }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width > height)
+            {
+                if (Search.IsVisible)
+                {
+                    Search.WidthRequest = width;
+                }
+            }
+        }
+
+        private void SearchButton_Clicked(object sender, EventArgs e)
+        {
+            this.Search.IsVisible = true;
+            this.Title.IsVisible = false;
+            this.SearchButton.IsVisible = false;
+
+            if (this.TitleView != null)
+            {
+                double opacity;
+
+                // Animating Width of the search box, from 0 to full width when it added to the view.
+                var expandAnimation = new Animation(
+                    property =>
+                    {
+                        Search.WidthRequest = property;
+                        opacity = property / TitleView.Width;
+                        Search.Opacity = opacity;
+                    }, 0, TitleView.Width, Easing.Linear);
+                expandAnimation.Commit(Search, "Expand", 16, 250, Easing.Linear, (p, q) => this.SearchExpandAnimationCompleted());
+            }
+        }
+
+        private void BackToTitle_Clicked(object sender, EventArgs e)
+        {
+            this.SearchButton.IsVisible = true;
+            if (this.TitleView != null)
+            {
+                double opacity;
+
+                // Animating Width of the search box, from full width to 0 before it removed from view.
+                var shrinkAnimation = new Animation(property =>
+                {
+                    Search.WidthRequest = property;
+                    opacity = property / TitleView.Width;
+                    Search.Opacity = opacity;
+                },
+                TitleView.Width, 0, Easing.Linear);
+                shrinkAnimation.Commit(Search, "Shrink", 16, 250, Easing.Linear, (p, q) => this.SearchBoxAnimationCompleted());
+            }
+
+            SearchEntry.Text = string.Empty;
+        }
+
+        private void SearchBoxAnimationCompleted()
+        {
+            this.Search.IsVisible = false;
+            this.Title.IsVisible = true;
+        }
+
+        private void SearchExpandAnimationCompleted()
+        {
+            this.SearchEntry.Focus();
+        }
     }
 }
