4edb7ee8e178763adf9bee7e8cb8cbf5faa3bffe
RusselJay27 <ponferrada.russel27@gmail.com>
2020-10-31 16:38:52 +0800

    Revised Mover data.

---
+++ PasaBuy.App/Http/HatidPress/MoverData.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using PasaBuy.App.Local;
+using PasaBuy.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace PasaBuy.App.Http.HatidPress
+{
+    public class MoverData
+    {
+        #region Fields
+
+        /// <summary>
+        /// Instance of Mover data.
+        /// </summary>
+        private static MoverData instance;
+        public static MoverData Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new MoverData();
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public MoverData()
+        {
+            client = new HttpClient();
+        }
+
+        #endregion
+
+        #region Method
+
+        /// <summary>
+        /// Get the information of mover using wpid.
+        /// </summary>
+        public async void GetData(Action<bool, string> callback)
+        {
+            try
+            {
+                var dict = new Dictionary<string, string>();
+                dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
+                dict.Add("snky", PSACache.Instance.UserInfo.snky);
+                var content = new FormUrlEncodedContent(dict);
+
+                var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/hatidpress/v2/mover/profile", content);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                    bool success = token.status == "success" ? true : false;
+                    string data = token.status == "success" ? result : token.message;
+                    callback(success, data);
+                }
+                else
+                {
+                    callback(false, "Network Error! Check your connection.");
+                }
+            }
+            catch (Exception e)
+            {
+                new Controllers.Notice.Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
+            }
+        }
+
+        #endregion
+    }
+}
