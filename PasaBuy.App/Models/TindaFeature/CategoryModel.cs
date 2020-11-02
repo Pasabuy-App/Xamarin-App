129c4b467a5d24385ca20955685fb25e89b4be87
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-02 22:12:39 +0800

    Fixed insert and uupdate of product category.

---
+++ PasaBuy.App/Models/TindaFeature/CategoryModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+
+namespace PasaBuy.App.Models.TindaFeature
+{
+    public class CategoryModel : INotifyPropertyChanged
+    {
+        public CategoryData[] data;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string id = string.Empty;
+        public string ID
+        {
+            get 
+            { 
+                return id; 
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("ID");
+            }
+        }
+
+        private string title = string.Empty;
+        public string Title
+        {
+            get 
+            { 
+                return title; 
+            }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private string info = string.Empty;
+        public string Info
+        {
+            get 
+            { 
+                return info; 
+            }
+            set
+            {
+                info = value;
+                OnPropertyChanged("Info");
+            }
+        }
+    }
+
+    public class CategoryData
+    {
+        public string ID = string.Empty;
+        public string title = string.Empty;
+        public string info = string.Empty;
+    }
+}
