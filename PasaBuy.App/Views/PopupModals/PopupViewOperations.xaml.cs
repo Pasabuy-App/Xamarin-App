0e8601523fdfec2f9a75022064c9a5b9cba7fc30
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-05 02:06:02 +0800

    Fixed operation details in store.

--- PasaBuy.App/Views/PopupModals/PopupViewOperations.xaml.cs
+++ PasaBuy.App/Views/PopupModals/PopupViewOperations.xaml.cs
@@ -15,41 +15,16 @@ namespace PasaBuy.App.Views.PopupModals
     public partial class PopupViewOperations : PopupPage
     {
         public static string opid;
+        public static string open;
+        public static string close;
+        public static string date;
         public PopupViewOperations()
         {
             InitializeComponent();
-            try
-            {
-                Operation.Instance.ListByID_Sales(PSACache.Instance.UserInfo.wpid, PSACache.Instance.UserInfo.snky, PSACache.Instance.UserInfo.stid, opid, (bool success, string data) =>
-                {
-                    if (success)
-                    {
-                        CultureInfo provider = new CultureInfo("fr-FR");
-                        Models.POSFeature.OperationModel datas = JsonConvert.DeserializeObject<Models.POSFeature.OperationModel>(data);
-                        for (int i = 0; i < datas.data.Length; i++)
-                        {
-                            string open_time = string.IsNullOrEmpty(datas.data[i].date_open) ? "0000-00-00 00:00:00" : datas.data[i].date_open;
-                            string close_time = string.IsNullOrEmpty(datas.data[i].date_close) ? "0000-00-00 00:00:00" : datas.data[i].date_close;
-                            string dates = string.IsNullOrEmpty(datas.data[i].date) ? "0000-00-00 00:00:00" : datas.data[i].date;
-                            DateTime open = DateTime.ParseExact(open_time, "yyyy-MM-dd HH:mm:ss", provider);
-                            DateTime close = DateTime.ParseExact(close_time, "yyyy-MM-dd HH:mm:ss", provider);
-                            DateTime date = DateTime.ParseExact(dates, "yyyy-MM-dd HH:mm:ss", provider);
 
-                            Opened.Text = open.ToString("hh:mm tt");
-                            Closed.Text = close.ToString("hh:mm tt");
-                            Date.Text = date.ToString("MMMM dd, yyyy");
-                        }
-                    }
-                    else
-                    {
-                        new Alert("Notice to User", HtmlUtils.ConvertToPlainText(data), "Try Again");
-                    }
-                });
-            }
-            catch (Exception e)
-            {
-                new Alert("Something went Wrong", "Please contact administrator. Error: " + e, "OK");
-            }
+            Opened.Text = open;
+            Closed.Text = close;
+            Date.Text = date;
         }
 
         private void CancelModal(object sender, EventArgs e)
