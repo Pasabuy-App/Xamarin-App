6338dda39ad86f2d7b93061cb93f29afb479da85
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-06 02:37:25 +0800

    Added variant in cart page lin order listing.

---
+++ PasaBuy.App/Models/HatidFeature/Delivery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasaBuy.App.Models.HatidFeature
+{
+    public class Delivery
+    {
+        public int data;
+    }
+}
