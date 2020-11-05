54942da0b2c78e615992ec54bf7694c6aaf8d87a
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-06 00:37:09 +0800

    Store payment info, transaction listing and change/select personnel.

--- PasaBuy.App/Http/MobilePOS/Wallet.cs
+++ PasaBuy.App/Http/MobilePOS/Wallet.cs
@@ -43,16 +43,16 @@ namespace PasaBuy.App.Http.MobilePOS
         #region Method
 
         /// <summary>
-        /// Listing of store wallet info and transactions list.
+        /// Store wallet info and transactions list.
         /// </summary>
         public async void Info(Action<bool, string> callback)
         {
             try
             {
                 var dict = new Dictionary<string, string>();
-                    dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
-                    dict.Add("snky", PSACache.Instance.UserInfo.snky);
-                    dict.Add("stid", PSACache.Instance.UserInfo.stid);
+                dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
+                dict.Add("snky", PSACache.Instance.UserInfo.snky);
+                dict.Add("stid", PSACache.Instance.UserInfo.stid);
                 var content = new FormUrlEncodedContent(dict);
 
                 var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/mobilepos/v2/wallets/info", content);
@@ -74,7 +74,44 @@ namespace PasaBuy.App.Http.MobilePOS
             }
             catch (Exception e)
             {
-                new Controllers.Notice.Alert("Something went Wrong", "Please contact administrator. Error Code: MPV2WLT-I1." + e, "OK");
+                new Controllers.Notice.Alert("Something went Wrong", "Please contact administrator. Error Code: MPV2WLT-I1.", "OK");
+            }
+        }
+
+        /// <summary>
+        /// Listing of store wallet info and transactions list.
+        /// </summary>
+        public async void Change(string key, string user_id, Action<bool, string> callback)
+        {
+            try
+            {
+                var dict = new Dictionary<string, string>();
+                    dict.Add("wpid", PSACache.Instance.UserInfo.wpid);
+                    dict.Add("snky", PSACache.Instance.UserInfo.snky);
+                    dict.Add("key", key);
+                    dict.Add("user_id", user_id);
+                var content = new FormUrlEncodedContent(dict);
+
+                var response = await client.PostAsync(PSAConfig.CurrentRestUrl + "/wp-json/mobilepos/v2/wallets/change", content);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                    bool success = token.status == "success" ? true : false;
+                    string data = token.status == "success" ? result : token.message;
+                    callback(success, data);
+                }
+                else
+                {
+                    callback(false, "Network Error! Check your connection.");
+                }
+            }
+            catch (Exception e)
+            {
+                new Controllers.Notice.Alert("Something went Wrong", "Please contact administrator. Error Code: MPV2WLT-C1.", "OK");
             }
         }
 
--- PasaBuy.App/ViewModels/MobilePOS/PaymentsViewModel.cs
+++ PasaBuy.App/ViewModels/MobilePOS/PaymentsViewModel.cs
@@ -17,7 +17,7 @@ namespace PasaBuy.App.ViewModels.MobilePOS
 {
     public class PaymentsViewModel : BaseViewModel
     {
-        private ObservableCollection<StorePayment> _walletTransactions;
+        public static ObservableCollection<StorePayment> _walletTransactions;
         public ObservableCollection<WalletCreditsModel> _walletCredits;
 
         public ObservableCollection<StorePayment> WalletTransactions
@@ -55,10 +55,14 @@ namespace PasaBuy.App.ViewModels.MobilePOS
         }
         private async void SetupClicked(string id)
         {
-            IsBusy = true;
-            await App.Current.MainPage.Navigation.PushModalAsync(new SearchWalletPersonnel());
-            await Task.Delay(500);
-            IsBusy = false;
+            if (!isRunning)
+            {
+                isRunning = true;
+                await Task.Delay(300);
+                SearchWalletPersonnel.key = this.AccountID;
+                await App.Current.MainPage.Navigation.PushModalAsync(new SearchWalletPersonnel());
+                isRunning = false;
+            }
 
         }
 
@@ -71,7 +75,13 @@ namespace PasaBuy.App.ViewModels.MobilePOS
         }
         private async void WithdrawClicked(string id)
         {
-            await PopupNavigation.Instance.PushAsync(new PopupStoreWithdraw());
+            if (!isRunning)
+            {
+                isRunning = true;
+                await Task.Delay(300);
+                await PopupNavigation.Instance.PushAsync(new PopupStoreWithdraw());
+                isRunning = false;
+            }
         }
         public bool _isRunning = false;
         public bool isRunning
@@ -131,7 +141,17 @@ namespace PasaBuy.App.ViewModels.MobilePOS
             _walletTransactions = new ObservableCollection<StorePayment>();
             //_walletCredits = new ObservableCollection<WalletCreditsModel>();
             LoadInfo();
+            _walletTransactions.CollectionChanged += CollectionChanges;
         }
+
+        private void CollectionChanges(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                LoadInfo();
+            }
+        }
+
         public void LoadInfo()
         {
             try
