07e4ff19d14eb99d58d406856099c98a97f868c9
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-13 12:31:16 +0800

    Added Permission Model./

---
+++ PasaBuy.App/Models/TindaFeature/PermissionModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasaBuy.App.Models.TindaFeature
+{
+    public class PermissionModel
+    {
+        public string action { get; set; }
+    }
+}
