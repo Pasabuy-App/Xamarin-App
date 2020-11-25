ea3f0e05d6f66d1dddc88c1c96bca17f148b5a72
RusselJay27 <ponferrada.russel27@gmail.com>
2020-11-25 17:16:00 +0800

    Fixed accept or inquire button in home direct to message.

--- PasaBuy.App/ViewModels/Feeds/HomepageViewModel.cs
+++ PasaBuy.App/ViewModels/Feeds/HomepageViewModel.cs
@@ -65,7 +65,7 @@ namespace PasaBuy.App.ViewModels.Feeds
 
         #region Constructor
         public Command<object> GoToProfileCommand { get; set; }
-        private async void OnTapped(object obj)
+        private void OnTapped(object obj)
         {
             var post = obj as Post;
             if (post.Post_author != PSACache.Instance.UserInfo.wpid)
@@ -76,8 +76,13 @@ namespace PasaBuy.App.ViewModels.Feeds
         public Command<object> ShareCommand { get; set; }
         private async void OnShared(object obj)
         {
-            var post = obj as Post;
-            await ShareUri(post.LinkPost);
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                var post = obj as Post;
+                await ShareUri(post.LinkPost);
+                IsRunning = false;
+            }
         }
         public async Task ShareUri(string uri)
         {
@@ -87,10 +92,23 @@ namespace PasaBuy.App.ViewModels.Feeds
             });
         }
         public Command<object> AcceptCommand { get; set; }
-        private void OnAccepted(object obj)
+        private async void OnAccepted(object obj)
         {
-            var post = obj as Post;
-            GetProfile(post.Post_author);
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                var post = obj as Post;
+
+                Chat.ChatMessageViewModel.refresh = 0;
+                Chat.ChatMessageViewModel.myPage = "home";
+                Chat.ChatMessageViewModel.odid = string.Empty;
+                Chat.ChatMessageViewModel.type = "user";
+                Chat.ChatMessageViewModel.ProfileNames = post.Author;
+                Chat.ChatMessageViewModel.ProfileImages = post.Photo;
+                Chat.ChatMessageViewModel.user_id = post.Post_author;
+                await App.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new Views.Chat.ChatMessagePage())); 
+                IsRunning = false;
+            }
         }
         public void GetData(string uid)
         {
